5cbbd70ff56dc9943729bef45af5e7b6576ab2e7
Kishore Arepalli <kishore.arepalli@gmail.com>
2017-08-01 14:53:20 +0200

    Validation missing in getMaxKeyWidth #3. Handle CreateQmlObject failure cases

--- js/ColumnHelper.js
+++ js/ColumnHelper.js
@@ -1,7 +1,9 @@
+var defaultMaxKeyWidth = 150
+
 function getMaxKeyWidth(list, parent)
 {
     if (list === null || parent === null)
-        return 0;
+        return defaultMaxKeyWidth;
 
     var maxColumnWidth = 0;
      for (var i = 0; i < list.length; i++)
@@ -9,17 +11,24 @@ function getMaxKeyWidth(list, parent)
         var data = list[i]
 
         if (data.length) {
-            var textElement = Qt.createQmlObject(
-            'import QtQuick 2.5;'
-            + 'import QtQuick.Controls 2.1;'
-            + 'Label {'
-            + ' text: "' + data + '"; '
-            + 'horizontalAlignment: Text.AlignRight '
-            + '}',
-            parent, "getMaxKeyWidth")
-            //console.log(data)
-            maxColumnWidth = Math.max(textElement.width, maxColumnWidth)
-            textElement.destroy()
+            try {
+                var textElement = Qt.createQmlObject(
+                'import QtQuick 5.5;'
+                + 'import QtQuick.Controls 2.1;'
+                + 'Label {'
+                + ' text: "' + data + '"; '
+                + 'horizontalAlignment: Text.AlignRight '
+                + '}',
+                parent, "getMaxKeyWidth")
+                //console.log(data)
+
+                maxColumnWidth = Math.max(textElement.width, maxColumnWidth)
+                textElement.destroy()
+            } catch(e) {
+                console.log(e)
+                maxColumnWidth = defaultMaxKeyWidth;
+                break;
+            }
          }
      }
      return maxColumnWidth
